7226c5fffc7b971b9592c1cc3a120566cf72af0d
hayachi-rin <hayachi884@gmail.com>
2024-10-12 14:42:38 +0900

    フッターの実装

--- wp/themes/hayatip/footer.php
+++ wp/themes/hayatip/footer.php
@@ -1,3 +1,6 @@
-<footer class="footer <?php if(!is_front_page()){echo 'footer--detail';}?>">&copy;2024.hayatip</footer>
+<footer class="footer <?php if(!is_front_page()){echo 'footer--detail';}?>">
+    <span class="copy">&copy;<?php echo date("Y");?>.hayatip</span>
+    <canvas id="sineCanvas" width="800" height="100"></canvas>
+</footer>
 </body>
 </html>
