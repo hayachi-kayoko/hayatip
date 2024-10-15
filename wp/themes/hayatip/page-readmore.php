4013be53748c385489cdbb443f4f31f1015c26c5
hayachi-rin <hayachi884@gmail.com>
2024-10-15 09:46:40 +0900

    非同期遷移

--- wp/themes/hayatip/page-readmore.php
+++ wp/themes/hayatip/page-readmore.php
@@ -11,37 +11,43 @@
     'posts_per_page' => $get_post_num,
     'offset' => $now_post_num,
   );
-  $posts = new WP_Query($args); ?>
+  echo $_SERVER['HTTP_HOST'];
+  if((strstr($_SERVER['REQUEST_URI'], '?', true)) === '/category/short/'){
+    $posts = new WP_Query('cat=3');
+  } else {
+    $posts = new WP_Query($args);
+  }
+  ?>
+  
   <?php if ($posts -> have_posts()) : ?>
-    <?php while($posts -> have_posts()) : $posts -> the_post(); ?>
- 
-<?php
-    $cat = get_the_category();
-    $cat = $cat[0];
-?>
-<?php
-//各種設定
-    $imgid = get_post_thumbnail_id();
-    $img = wp_get_attachment_image_src($imgid, 'large');
-    $title = the_title( '', '', false );
-    $price = get_field('price');
-    $mercari = get_field('mercari');
-    $url = esc_url(get_permalink());
-    $name = $post->post_name;
-?>
+   <?php while($posts -> have_posts()) : $posts -> the_post(); ?>
 
-<?php
-//ヒアドキュメントでループ内の内容を変数に格納
-echo '<article class="main-archive_inner_item js-item js-items is-hidden is-' . $name . ' is-' . $cat->slug . '"data-name="' . $name . '">';
-echo '<a href="' . $url . '">';
-echo '<span class="main-archive_inner_item_category main-archive_inner_item_category--' . $cat->slug . '";><span>' . $cat->name . '</span></span>';
-echo '<figure class="main-archive_inner_item_img js-archive-img"><img src="' . $img[0] . '" alt="' . $title . '"></figure>';
-echo '<h3 class="main-archive_inner_item_title">' . $title . '</h3>';
-echo '<p class="main-archive_inner_item_price"><span>¥</span>' . $price . '</p>';
-echo '<p class="main-archive_inner_item_price"><i>メルカリ→</i><span>¥</span>' . $mercari . '</p>';
-echo '</a>';
-echo '</article>';
-?>
+  <?php
+      $cat = get_the_category();
+      $cat = $cat[0];
+  ?>
+  <?php
+  //各種設定
+      $imgid = get_post_thumbnail_id();
+      $img = wp_get_attachment_image_src($imgid, 'large');
+      $title = the_title( '', '', false );
+      $price = get_field('price');
+      $mercari = get_field('mercari');
+      $url = esc_url(get_permalink());
+      $name = $post->post_name;
+  ?>
 
+  <?php
+  //ヒアドキュメントでループ内の内容を変数に格納
+  echo '<article class="main-archive_inner_item js-item js-items is-hidden is-' . $name . ' is-' . $cat->slug . '"data-name="' . $name . '">';
+  echo '<a href="' . $url . '">';
+  echo '<span class="main-archive_inner_item_category main-archive_inner_item_category--' . $cat->slug . '";><span>' . $cat->name . '</span></span>';
+  echo '<figure class="main-archive_inner_item_img js-archive-img"><img src="' . $img[0] . '" alt="' . $title . '"></figure>';
+  echo '<h3 class="main-archive_inner_item_title">' . $title . '</h3>';
+  echo '<p class="main-archive_inner_item_price"><span>¥</span>' . $price . '</p>';
+  echo '<p class="main-archive_inner_item_price"><i>メルカリ→</i><span>¥</span>' . $mercari . '</p>';
+  echo '</a>';
+  echo '</article>';
+  ?>
 <?php endwhile; ?>
 <?php endif; wp_reset_postdata(); ?>
